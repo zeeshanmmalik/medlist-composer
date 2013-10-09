fd04ee4a3bc533a5f0474d3662a7142d001c55d9
Zeeshan Maqbool <zeeshan.m.malik@gmail.com>
2013-10-09 07:11:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DischargeMedlistComposer::Application.config.secret_key_base = '2c1053ec59bda6912806d2e7d3f25b493632d7e3e87bad5571766b0102670ca5bd9fe59262f3652a10bc82904fe150747fc75095a3497c95706cadbabf20818c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DischargeMedlistComposer::Application.config.session_store :cookie_store, key: '_discharge_medlist_composer_session'
