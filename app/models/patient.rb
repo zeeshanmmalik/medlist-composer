8c59c7dd1030a699af4fdc52e7a4cba9a212a847
Zeeshan Maqbool <zeeshan.m.malik@gmail.com>
2013-11-04 12:21:49 -0500

    mrn made unique

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,8 @@
 class Patient < ActiveRecord::Base
   has_many :prescriptions
 
+  validates :mrn, presence: true, uniqueness: true
+
   def name
     "#{last_name.capitalize}, #{first_name.capitalize}"
   end
